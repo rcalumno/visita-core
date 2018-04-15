8f2c6b04c70a234cf36cc349de3b5ccce3ccec21
Roberto <maniac787@gmail.com>
2018-04-15 10:11:59 -0700

    Singleton engine to read excel file

---
+++ src/visita_engine.py
@@ -0,0 +1,21 @@
+class VisitaEngine:
+    # Here will be the instance stored.
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if VisitaEngine.__instance is None:
+            VisitaEngine()
+        return VisitaEngine.__instance
+
+    def __init__(self):
+        """ Virtually private constructor. """
+        if VisitaEngine.__instance is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            VisitaEngine.__instance = self
+
+
+    def leer_achivo(self):
+        print ("leyendo archivo")
