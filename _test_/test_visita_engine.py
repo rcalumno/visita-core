bf1c6e77eabecf0935a1316c670546992a1527f4
Roberto <maniac787@gmail.com>
2018-04-15 19:17:58 -0700

    Add pandas framework to iterate xlsx data

--- _test_/test_visita_engine.py
+++ _test_/test_visita_engine.py
@@ -6,6 +6,6 @@ from visita_engine import VisitaEngine
 class VisitaTestCase(unittest.TestCase):
     def test_visita_engine_existen_hojas(self):
         excel_archive = VisitaEngine.getInstance().leer_achivo(
-            ".././src/resources/VISITA_TEST.xlsx")
+            "VISITA_TEST.xlsx")
         self.assertIsNotNone(excel_archive)
         print(excel_archive)
