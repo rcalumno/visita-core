320ac4dc49e3c5a491dedfaaeb1d6bac43c768f5
Roberto <maniac787@gmail.com>
2018-06-09 01:15:39 -0500

    Creacion escenarios

--- _test_/test_escenario.py
+++ _test_/test_escenario.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 import pandas as pd
 import pymongo
@@ -7,17 +8,17 @@ from pymongo import MongoClient
 
 
 class EscenarioTestCase(unittest.TestCase):
+    client = MongoClient("mongodb://localhost:27017/")
+    client.visita_db.authenticate('roberto', 'admin', mechanism='SCRAM-SHA-1')
+    database = client["visita_db"]
+    collection = database["visita_col"]
 
     def test_transacciones_mes(self):
-        client = MongoClient("mongodb://localhost:27017/")
-        client.visita_db.authenticate('roberto', 'admin', mechanism='SCRAM-SHA-1')
-        database = client["visita_db"]
-        collection = database["visita_col"]
         query = {}
         query["RUC"] = Int64(100373885001)
 
         # informacion de la base
-        cursor = collection.find(query).sort([['MES', pymongo.ASCENDING], ['TIPO_TRANSACCION', pymongo.ASCENDING]])
+        cursor = self.collection.find(query).sort([['MES', pymongo.ASCENDING], ['TIPO_TRANSACCION', pymongo.ASCENDING]])
         # data frame en pandas
         data = pd.DataFrame(list(cursor))
 
@@ -25,6 +26,52 @@ class EscenarioTestCase(unittest.TestCase):
 
         print(data_filtrado)
 
-        conteo = data_filtrado.groupby(['MES', 'TIPO_TRANSACCION']).count()
+        conteo = data_filtrado.groupby(['MES', 'NOMBRE', 'TIPO_TRANSACCION']).count()
+
+        print(conteo)
+
+    def test_costo_mes(self):
+        query = {}
+        query["RUC"] = Int64(100373885001)
+
+        # informacion de la base
+        cursor = self.collection.find(query).sort(
+            [['MES', pymongo.ASCENDING], ['TIPO_TRANSACCION', pymongo.ASCENDING]])
+        # data frame en pandas
+        data = pd.DataFrame(list(cursor))
+
+        data_filtrado = data[['MES', 'NOMBRE', 'TIPO_TRANSACCION', 'COSTO']]
+
+        print(data_filtrado)
+
+    def test_monto_mes(self):
+        query = {}
+        query["RUC"] = Int64(100373885001)
+
+        # informacion de la base
+        cursor = self.collection.find(query).sort(
+            [['MES', pymongo.ASCENDING], ['TIPO_TRANSACCION', pymongo.ASCENDING]])
+        # data frame en pandas
+        data = pd.DataFrame(list(cursor))
+
+        data_filtrado = data[['MES', 'NOMBRE', 'TIPO_TRANSACCION', 'MONTO']]
+
+        print(data_filtrado)
+
+    def test_transacciones_provincia_mes(self):
+        query = {}
+        # query["RUC"] = Int64(100373885001)
+        query["MES"] = datetime(2017, 1, 1, 0, 0, 0)
+
+        # informacion de la base
+        cursor = self.collection.find(query)
+        # data frame en pandas
+        data = pd.DataFrame(list(cursor))
+
+        data_filtrado = data[['PROVINCIA', 'MES', 'NOMBRE', 'TIPO_TRANSACCION', 'MONTO']]
+
+        print(data_filtrado)
+
+        conteo = data_filtrado.groupby(['PROVINCIA', 'MES', 'NOMBRE', 'TIPO_TRANSACCION']).count()
 
         print(conteo)
